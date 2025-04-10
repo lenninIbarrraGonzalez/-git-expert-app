74d46cd76b6d7950ccc0163aa03a9b87545875b4
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-04-10 17:31:04 -0500

    feature/agregando archivo de estilos y boton con llamado a funcion para agregar categorias

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,7 +1,28 @@
+import { useState } from "react"
+
 export const GifExpertApp = () => {
+
+    const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
+    console.log(categories)
+
+    const onAddCategory = () => {
+        setCategories([...categories, "valorant"]);
+    }
+
     return (
     <>
+        {/* title */}
         <h1>GifExpertApp</h1>
+
+        {/* listado de gifs */}
+            <button onClick={onAddCategory}>Agregar</button>
+            <ol>
+                {categories.map((category)=>{
+                    return <li key={category}>{category}</li>
+                })}
+            </ol>
+
+            {/* gif itmes  */}
     </>
     )
 }
