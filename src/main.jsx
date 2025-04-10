10c358495a6fcf49e9de42af14d3c79784f0769d
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-04-10 16:41:44 -0500

    Inicio del proyecto, se implementa el punto de entrada y se eliminan archivos que no se utilzan

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { GifExpertApp } from './GifExpertApp';
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <GifExpertApp />
+  </StrictMode>,
+)
+
+
+// SliKgWRHIVXb3IC7B79tXwYLZxlJD9Eh
