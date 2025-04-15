7d323304fe36813c2d3b32d4d040e73b91fcd4f0
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-04-15 17:24:36 -0500

    feature/se agrega componente GifGrid

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,33 +1,22 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
 
     const [categories, setCategories] = useState(['One Punch', 'Dragon Ball']);
 
     const onAddCategory = (onNewCategory) => {
-        const index = categories.findIndex((categorie) => categorie === onNewCategory);
+        const index = categories.findIndex((category) => category === onNewCategory);
         if(index > -1) return;
         setCategories([...categories, onNewCategory]);
     }
 
     return (
     <>
-        {/* title */}
         <h1>GifExpertApp</h1>
-        
         <AddCategory onNewCategory={onAddCategory}/>
-
-        {/* listado de gifs */}
-            <ol>
-                {/* {categories.map((category)=>{
-                    return <li key={category}>{category}</li>
-                })} */}
-
-                {categories.map(category =><li key={category}>{category}</li>)}
-            </ol>
-
-            {/* gif itmes  */}
+        {categories.map(category => <GifGrid key = {category} category={category}/> )}
     </>
     )
 }
