94f2c362cbd6863eb7dc101e649dfd00e4790a23
Lennin Ibarra <ing.lenninibarra@gmail.com>
2025-04-18 11:32:24 -0500

    feature/implementando un loading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -4,11 +4,13 @@ import { GifItem } from "./GifItem";
 export const GifGrid = ({category}) => {
 
     const {images, isLoading} = useFetchGifs(category);
-    
-    
+        
     return (
         <>
             <h3>{category}</h3>
+
+            {isLoading && <span>Cargando...</span>}
+            
             <div className="card-grid">
             {images.map((images) => (
                 <GifItem 
